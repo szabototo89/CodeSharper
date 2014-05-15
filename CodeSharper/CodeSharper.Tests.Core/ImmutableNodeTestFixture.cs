d4f5b160ed6ea6f37991a824e5b16945802859c6
szabototo89 <szabototo89@gmail.com>
2014-05-15 20:34:22 +0200

    Added a new test

---
+++ CodeSharper/CodeSharper.Tests.Core/ImmutableNodeTestFixture.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CodeSharper.Tests.Core
+{
+    [TestFixture]
+    class ImmutableNodeTestFixture
+    {
+        [Test]
+        public void ImmutableNodeTest()
+        {
+            // GIVEN
+
+            // WHEN
+
+            // THEN
+
+        }
+    }
+}
