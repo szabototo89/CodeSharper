b390833d35eea6d00db891f3f7c6a5b082da3d8f
szabototo89 <szabototo89@gmail.com>
2014-06-01 16:27:04 +0200

    CodeSharper/CodeSharper.Core/Csv/Factories/ICsvNodeFactory   - Moved to new file

---
+++ CodeSharper/CodeSharper.Core/Csv/Factories/ICsvNodeFactory.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using CodeSharper.Core.Csv.Nodes;
+
+namespace CodeSharper.Core.Csv.Factories
+{
+    public interface ICsvNodeFactory
+    {
+        FieldNode Field(string value);
+        RecordNode Record(IEnumerable<FieldNode> fields);
+        CsvCompilationUnit CompilationUnit(RecordNode[] records);
+        CommaNode Comma();
+    }
+}
