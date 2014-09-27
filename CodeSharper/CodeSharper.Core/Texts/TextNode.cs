d78a13048f41cdf401243451cd692b0d805edd6a
Tamas Szabo <szabototo89@gmail.com>
2014-09-27 20:32:50 +0100

    Started to implement text commands to CodeSharper. CodeSharper/CodeSharper.Core/Texts:   - Added TextNode class to Texts namespace, which represents nodes of text (TextNode.cs)   - Added test methods to TextNode class (TextNodeTestFixture.cs)   - Created a TextNodeHelper class for extensions methods (TextNodeHelper.cs)

---
+++ CodeSharper/CodeSharper.Core/Texts/TextNode.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeSharper.Core.Common.ConstraintChecking;
+
+namespace CodeSharper.Core.Texts
+{
+    public class TextNode
+    {
+        public string Text { get; protected set; }
+
+        public TextSpan TextSpan { get; protected set; }
+
+        public TextNode Parent { get; protected set; }
+
+
+
+        public TextNode(String text, TextNode parent = null)
+        {
+            Constraints.NotNull(() => text);
+
+            Text = text;
+            Parent = parent;
+            TextSpan = new TextSpan(text);
+        }
+
+        public TextNode GetSubText(Int32 start, Int32 end)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CodeSharper/CodeSharper.Tests.Core/Texts/TextNodeTestFixture.cs
@@ -0,0 +1,63 @@
+using CodeSharper.Core.Texts;
+using NUnit.Framework;
+
+namespace CodeSharper.Tests.Core.Texts
+{
+    [TestFixture]
+    internal class TextNodeTestFixture
+    {
+        [Test(Description = "TextNode should have Text and Position property")]
+        public void TextNodeShouldHaveTextAndPositionProperty()
+        {
+            // Given
+            var underTest = new TextNode("Hello World!");
+
+            // When
+            var result = new
+            {
+                underTest.Text,
+                underTest.TextSpan
+            };
+
+            // Then
+            var expected = new
+            {
+                Text = "Hello World!",
+                TextSpan = new TextSpan(0, "Hello World!")
+            };
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void ThisTextNodeHaveNotParentBecauseItIsRootNode()
+        {
+            // Given
+            var underTest = new TextNode("Hello World!");
+
+            // When
+            var result = new
+            {
+                Parent = underTest.Parent, 
+                IsRoot = underTest.IsRoot()
+            };
+
+            // Then
+            Assert.That(result.Parent, Is.Null);
+            Assert.That(result.IsRoot, Is.True);
+        }
+
+        [Test]
+        public void TextNodeShouldGetSubText()
+        {
+            // Given
+            var underTest = new TextNode("Hello World!");
+
+            // When
+            var result = underTest.GetSubText(0, 4);
+
+            // Then
+            Assert.That(result, Is.TypeOf<TextNode>());
+        }
+    }
+}
