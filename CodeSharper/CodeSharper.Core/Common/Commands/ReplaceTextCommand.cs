0596fe79d50fed8ea511cf0f609c1e480b8988e9
Tamas Szabo <szabototo89@gmail.com>
2014-09-28 11:23:36 +0100

    Added Moq library to testing solution to making mock object easily. CodeSharper/CodeSharper.Core/Common/Commands/ReplaceTextCommand:   - Added ReplaceTextCommand to commands   - You can easily replace text in text nodes

---
+++ CodeSharper/CodeSharper.Core/Common/Commands/ReplaceTextCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CodeSharper.Core.Common.Commands
+{
+    public class ReplaceTextCommand : StringTransformationCommand
+    {
+        public ReplaceTextCommand(String replacedText)
+            : base(text => ReplaceText(replacedText))
+        {
+        }
+
+        private static String ReplaceText(String replacedText)
+        {
+            return replacedText;
+        }
+    }
+}
