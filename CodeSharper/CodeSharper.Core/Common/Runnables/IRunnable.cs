fd861826b8e359c641cae24adf9d2b701e7156d2
Tamas Szabo <szabototo89@gmail.com>
2014-10-15 22:58:15 +0100

    Little refactoring in IRunnable.cs file. Runnable class moved to Runnable.cs.

--- CodeSharper/CodeSharper.Core/Common/Runnables/IRunnable.cs
+++ CodeSharper/CodeSharper.Core/Common/Runnables/IRunnable.cs
@@ -20,40 +20,4 @@ namespace CodeSharper.Core.Common.Runnables
     {
         TOut Run(TIn parameter);
     }
-
-    // TODO: Refactor this section (Consumes/Produces) to another static class
-    public abstract class Runnable<TIn, TOut> : IRunnable<TIn, TOut>
-    {
-        private readonly List<IArgumentWrapper> _supportedArgumentWrappers;
-        private readonly List<IArgumentUnwrapper> _supportedArgumentUnwrappers; 
-
-        public IEnumerable<IArgumentWrapper> SupportedArgumentWrappers { get { return _supportedArgumentWrappers.AsReadOnly(); } }
-
-        public IEnumerable<IArgumentUnwrapper> SupportedArgumentUnwrappers { get { return _supportedArgumentUnwrappers.AsReadOnly(); } }
-
-        protected void Consumes<TArgumentUnwrapper>()
-            where TArgumentUnwrapper : IArgumentUnwrapper, new()
-        {
-            _supportedArgumentUnwrappers.Add(new TArgumentUnwrapper());
-        }
-
-        protected void Produces<TArgumentWrapper>() 
-            where TArgumentWrapper : IArgumentWrapper, new()
-
-        {
-            _supportedArgumentWrappers.Add(new TArgumentWrapper());
-        }
-        protected Runnable()
-        {
-            _supportedArgumentWrappers = new List<IArgumentWrapper>();
-            _supportedArgumentUnwrappers =  new List<IArgumentUnwrapper>();
-        }
-
-        public abstract TOut Run(TIn parameter);
-
-        Object IRunnable.Run(Object parameter)
-        {
-            return Run((TIn)parameter);
-        }
-    }
 }
