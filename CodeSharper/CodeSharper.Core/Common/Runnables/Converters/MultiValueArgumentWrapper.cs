0c5ac172284f98a33385a22f35a72f68d92a2292
Tamas Szabo <szabototo89@gmail.com>
2014-10-18 23:49:01 +0100

    [refactoring] Changing back the previous version.

--- CodeSharper/CodeSharper.Core/Common/Runnables/Converters/MultiValueArgumentWrapper.cs
+++ CodeSharper/CodeSharper.Core/Common/Runnables/Converters/MultiValueArgumentWrapper.cs
@@ -10,20 +10,6 @@ namespace CodeSharper.Core.Common.Runnables.Converters
     public class MultiValueArgumentWrapper<TParameter> 
         : ArgumentWrapper<IEnumerable<TParameter>, MultiValueArgument<TParameter>>
     {
-        public override Boolean IsWrappable(Object parameter)
-        {
-            if (base.IsWrappable(parameter))
-                return true;
-
-            var array = parameter as IEnumerable;
-            return array != null && array.OfType<TParameter>().Any();
-        }
-
-        public override Object Wrap(Object parameter)
-        {
-            return Wrap(((IEnumerable)parameter).Cast<TParameter>());
-        }
-
         public override MultiValueArgument<TParameter> Wrap(IEnumerable<TParameter> parameter)
         {
             return Arguments.MultiValue(parameter);
