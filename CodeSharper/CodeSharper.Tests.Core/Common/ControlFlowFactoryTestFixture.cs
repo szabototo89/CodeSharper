47d015b9a49b8c71e1ad9d480fe4116a347b823b
szabototo89 <szabototo89@gmail.com>
2014-11-28 23:59:52 +0100

    CodeSharper.Tests.Core/Common/ControlFlowFactoryTestFixture: - Add a ControlFlowFactoryTestFixture test fixture for testing new ControlFlowFactories

---
+++ CodeSharper/CodeSharper.Tests.Core/Common/ControlFlowFactoryTestFixture.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeSharper.Core.Commands;
+using NUnit.Framework;
+
+namespace CodeSharper.Tests.Core.Common
+{
+    [TestFixture]
+    internal class ControlFlowFactoryTestFixture
+    {
+        [SetUp]
+        public void Setup() { }
+
+        [TearDown]
+        public void Teardown() { }
+
+        [Test]
+        public void StandardControlFlowFactoryShouldAbleToParseCommandCallAndBuildAnControlFlowFromIt()
+        {
+            // Given
+            ICommandCall commandCall;
+            var underTest = new StandardControlFlowFactory();
+
+            // When
+            var result = underTest.Parse(commandCall);
+
+            // Then
+
+        }
+
+    }
+}
