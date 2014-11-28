dbfdcd8d3cc19179ff7a04dcce53c5c4e1c7221a
szabototo89 <szabototo89@gmail.com>
2014-11-29 00:06:58 +0100

    CodeSharper.Tests.Core/Common/TestFixtureBase: - Add a common base abstract test fixture class

---
+++ CodeSharper/CodeSharper.Tests.Core/Common/TestFixtureBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Ninject;
+
+namespace CodeSharper.Tests.Core.Common
+{
+    internal class TestFixtureBase : IDisposable
+    {
+        protected IKernel Kernel;
+
+        public TestFixtureBase()
+        {
+            Kernel = new StandardKernel();
+        }
+
+        public void Dispose()
+        {
+            if (!Kernel.IsDisposed)
+                Kernel.Dispose();
+        }
+    }
+}
