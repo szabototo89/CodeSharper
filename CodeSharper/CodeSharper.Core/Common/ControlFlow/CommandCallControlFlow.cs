3aec6765d2d6b9f5933bd958543625dc33a7537d
Tamas Szabo <szabototo89@gmail.com>
2014-12-31 14:56:39 +0100

    Add CommandCallControlFlow class and tests to represent single command call. CodeSharper.Core/Common/ControlFlow/CommandCallControlFlow and CommandCallControlFlowTestFixture:  - Create and test CommandCallControlFlow class for executing one command

---
+++ CodeSharper/CodeSharper.Core/Common/ControlFlow/CommandCallControlFlow.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeSharper.Core.Commands;
+using CodeSharper.Core.Commands.CommandFactories;
+using CodeSharper.Core.Common.ConstraintChecking;
+using CodeSharper.Core.Common.Values;
+
+namespace CodeSharper.Core.Common.ControlFlow
+{
+    public class CommandCallControlFlow : IControlFlow
+    {
+        public IExecutor Executor { get; private set; }
+        public ICommand Command { get; private set; }
+
+        public CommandCallControlFlow(IExecutor executor, ICommand command)
+        {
+            Constraints.NotNull(() => executor);
+            Constraints.NotNull(() => command);
+
+            Executor = executor;
+            Command = command;
+        }
+
+        public Argument Execute(Argument parameter)
+        {
+            return Executor.Execute(Command.Runnable, parameter);
+        }
+    }
+}
---
+++ CodeSharper/CodeSharper.Tests.Core/Common/ControlFlow/CommandCallControlFlowTestFixture.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeSharper.Core.Commands;
+using CodeSharper.Core.Common.ControlFlow;
+using CodeSharper.Core.Common.Values;
+using CodeSharper.Tests.Core.Mocks;
+using CodeSharper.Tests.Core.TestHelpers;
+using NUnit.Framework;
+
+namespace CodeSharper.Tests.Core.Common.ControlFlow
+{
+    [TestFixture]
+    internal class CommandCallControlFlowTestFixture : TestFixtureBase
+    {
+        [Test(Description = "CommandCallControlFlow should be able to execute any command when pass executor and command")]
+        public void CommandCallControlFlowShouldBeAbleToExecuteAnyCommandWhenPassExecutorAndCommand()
+        {
+            // Given
+            var executor = ExecutorMocks.SimpleExecutor<Int32>();
+            var command = CommandFactoryMocks.DelegateCommandFactory("test", num => num + 1)
+                .CreateCommand(new CommandArgumentCollection());
+
+            var underTest = new CommandCallControlFlow(executor, command);
+
+            // When
+            var result = underTest.Execute(Arguments.Value(10));
+
+            // Then
+            Assert.That(result, Is.InstanceOf<ValueArgument<Object>>());
+            Assert.That(((ValueArgument<Object>)result).Value, Is.EqualTo(11));
+        }
+    }
+}
