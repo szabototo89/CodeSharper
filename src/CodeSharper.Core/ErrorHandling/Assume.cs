d321b6565c1ecf17151dc05296150b0aa179e4a0
Tamas Szabo <szabototo89@gmail.com>
2015-02-07 11:15:37 +0100

    Add DebuggerStepThrough attributes to every method in Assume class

--- src/CodeSharper.Core/ErrorHandling/Assume.cs
+++ src/CodeSharper.Core/ErrorHandling/Assume.cs
@@ -1,21 +1,25 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace CodeSharper.Core.ErrorHandling
 {
     public static class Assume
     {
+        [DebuggerStepThrough]
         public static void NotNull<TValue>(TValue value, String parameter) where TValue : class
         {
             if (value == null)
                 throw new ArgumentNullException(parameter, String.Format("{0} cannot be null!", parameter));
         }
 
+        [DebuggerStepThrough]
         public static void IsTrue(Boolean condition, String message)
         {
             if (!condition)
                 throw new Exception(message);
         }
 
+        [DebuggerStepThrough]
         public static void IsTrue(Boolean condition, Func<Exception> exception)
         {
             if (!condition)
