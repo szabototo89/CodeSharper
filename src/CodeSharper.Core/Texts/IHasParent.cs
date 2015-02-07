0b0dc4e8bfbe44d700c48a9f753f1f1b2b30dbc2
Tamas Szabo <szabototo89@gmail.com>
2015-02-07 11:19:51 +0100

    Add IHasParent interface for representing that object which has parent

---
+++ src/CodeSharper.Core/Texts/IHasParent.cs
@@ -0,0 +1,10 @@
+﻿namespace CodeSharper.Core.Texts
+{
+    public interface IHasParent<out TParent>
+    {
+        /// <summary>
+        /// Gets parent of current text range
+        /// </summary>
+        TParent Parent { get; }
+    }
+}
