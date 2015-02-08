619fff0f9983e9f73b9eb9288f8104ce7e9887d6
Tamas Szabo <szabototo89@gmail.com>
2015-02-08 22:10:33 +0100

    Add a static class which contains extension methods for enumerable types

---
+++ src/CodeSharper.Core/Utilities/EnumerableExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeSharper.Core.Common;
+using CodeSharper.Core.ErrorHandling;
+
+namespace CodeSharper.Core.Utilities
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<TResult> Select<TElement, TResult>(this TElement element, Func<TElement, TResult> func)
+            where TElement : IHasNext<TElement>
+        {
+            Assume.NotNull(func, "func");
+
+            var current = element;
+
+            while (current != null)
+            {
+                yield return func(current);
+                current = current.Next;
+            }
+        }
+
+        public static IEnumerable<TElement> AsEnumerable<TElement>(this TElement element)
+            where TElement : IHasNext<TElement>
+        {
+            var current = element;
+
+            while (current != null)
+            {
+                yield return current;
+                current = current.Next;
+            }
+        }
+    }
+}
