f4e773012f768256a99abf4c5d9b15b07c3a760a
Tamas Szabo <szabototo89@gmail.com>
2015-07-06 16:59:55 +0200

    [core] Refactor TextDocument to universal interface #46

---
+++ src/CodeSharper.Core/TextManagers/ITextManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CodeSharper.Core.TextManagers
+{
+    public interface ITextManager
+    {
+        /// <summary>
+        /// Gets the value.
+        /// </summary>
+        String GetValue(TextMarkerBase marker);
+
+        /// <summary>
+        /// Sets the value.
+        /// </summary>
+        void SetValue(TextMarkerBase marker, String value);
+
+        /// <summary>
+        /// Creates the marker.
+        /// </summary>
+        TextMarkerBase CreateMarker(Int32 start, Int32 exclusiveStop);
+
+        /// <summary>
+        /// Removes the marker.
+        /// </summary>
+        Boolean RemoveMarker(TextMarkerBase marker);
+    }
+}
---
+++ src/CodeSharper.Core/TextManagers/TextMarker.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace CodeSharper.Core.TextManagers
+{
+    public abstract class TextMarkerBase : IEquatable<TextMarkerBase>
+    {
+        private readonly Guid id;
+
+        /// <summary>
+        /// Gets the identifier.
+        /// </summary>
+        public Guid Id
+        {
+            get { return id; }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TextMarkerBase"/> class.
+        /// </summary>
+        protected TextMarkerBase()
+        {
+            id = Guid.NewGuid();
+        }
+
+        /// <summary>
+        /// Indicates whether the current object is equal to another object of the same type.
+        /// </summary>
+        /// <returns>
+        /// true if the current object is equal to the <paramref name="other"/> parameter; otherwise, false.
+        /// </returns>
+        /// <param name="other">An object to compare with this object.</param>
+        public Boolean Equals(TextMarkerBase other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return id.Equals(other.id);
+        }
+
+        /// <summary>
+        /// Determines whether the specified <see cref="T:System.Object"/> is equal to the current <see cref="T:System.Object"/>.
+        /// </summary>
+        /// <returns>
+        /// true if the specified object  is equal to the current object; otherwise, false.
+        /// </returns>
+        /// <param name="obj">The object to compare with the current object. </param>
+        public override Boolean Equals(Object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((TextMarkerBase) obj);
+        }
+
+        /// <summary>
+        /// Serves as a hash function for a particular type. 
+        /// </summary>
+        /// <returns>
+        /// A hash code for the current <see cref="T:System.Object"/>.
+        /// </returns>
+        public override Int32 GetHashCode()
+        {
+            return id.GetHashCode();
+        }
+
+        /// <summary>
+        /// Returns a string that represents the current object.
+        /// </summary>
+        /// <returns>
+        /// A string that represents the current object.
+        /// </returns>
+        public override String ToString()
+        {
+            return String.Format("TextMarker({0})", Id);
+        }
+    }
+}
