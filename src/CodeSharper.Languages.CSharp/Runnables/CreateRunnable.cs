0bf252b13d44304913ff227e27d96af63e678d77
Tamas Szabo <szabototo89@gmail.com>
2015-07-26 11:00:28 +0200

    Start to implement object context into queries.

--- src/CodeSharper.Languages.CSharp/Runnables/CreateRunnable.cs
+++ src/CodeSharper.Languages.CSharp/Runnables/CreateRunnable.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using CodeSharper.Core.Common.Runnables;
+using CodeSharper.Core.Common.Runnables.Attributes;
+using CodeSharper.Core.Common.Runnables.Converters;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Editing;
+using Microsoft.CodeAnalysis.Text;
 
 namespace CodeSharper.Languages.CSharp.Runnables
 {
@@ -16,6 +20,34 @@ namespace CodeSharper.Languages.CSharp.Runnables
         public override SyntaxNode Run(Object parameter)
         {
             return CSharpSyntaxTree.ParseText(Source).GetRoot();
+        }
+    }
+
+    [Consumes(typeof(MultiValueConsumer<SyntaxNode>))]
+    public class CreateCommentRunnable : RunnableBase<SyntaxNode, SyntaxNode>
+    {
+        /// <summary>
+        /// Runs an algorithm with the specified parameter.
+        /// </summary>
+        public override SyntaxNode Run(SyntaxNode parameter)
+        {
+            if (parameter == null) return null;
+
+            var sourceText = parameter.WithoutTrivia().ToFullString();
+            var commentedSourceText = String.Format("/* {0} */", sourceText);
+            AdhocWorkspace workspace = new AdhocWorkspace();
+            var document = workspace.CurrentSolution.GetDocument(parameter.SyntaxTree);
+            var editor = DocumentEditor.CreateAsync(document).Result;
+
+            // handle root element
+            if (parameter.Parent == null)
+                return parameter.SyntaxTree.WithChangedText(SourceText.From(commentedSourceText)).GetRoot();
+
+            // if it is not root element then insert after it and remove the original one
+            var comment = SyntaxFactory.Comment(commentedSourceText);
+            // var commentedNode = SyntaxFactory.MissingToken(SyntaxKind.EmptyStatement).WithLeadingTrivia(comment);
+
+            return parameter.WithLeadingTrivia(comment);
         }
     }
 }
---
+++ tests/CodeSharper.Tests.Languages/CSharp/Runnables/RunnableTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+using CodeSharper.Core.Utilities;
+using CodeSharper.Languages.CSharp.Runnables;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Formatting;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Formatting;
+using Microsoft.CodeAnalysis.Options;
+using NUnit.Framework;
+
+namespace CodeSharper.Tests.Languages.CSharp.Runnables
+{
+    [TestFixture]
+    internal class RunnableTests
+    {
+        private SyntaxNode FormatSyntaxNode(SyntaxNode node)
+        {
+            Workspace customWorkspace = new AdhocWorkspace();
+            OptionSet options = customWorkspace.Options;
+            var formattedResult = Formatter.Format(node, customWorkspace, options);
+            return formattedResult;
+        }
+
+        [Test(Description = "CreateCommentRunnable should remove node from tree and insert a comment when getting syntax node")]
+        public void CreateCommentRunnable_ShouldRemoveNodeFromTreeAndInsertAComment_WhenGettingSyntaxNode()
+        {
+            // Given
+            var source = @"
+                public void foo() {
+                    var a = 10;
+                }
+            ";
+            var node = SyntaxFactory.ParseSyntaxTree(source).GetRoot();
+            var underTest = new CreateCommentRunnable();
+
+            // When
+            var result = underTest.Run(node).ToFullString();
+
+            // Then
+            Assert.That(result, Is.Not.Null);
+        }
+
+        [Test(Description = "CreateCommentRunnable should remove node from tree and insert a comment when getting syntax node")]
+        public void CreateCommentRunnable_ShouldRemoveNodeFromTreeAndInsertAComment_WhenTransformingMethodBody()
+        {
+            // Given
+            var source = @"
+                public void foo() {
+                    var a = 10;
+                }
+            ";
+            var compilationUnit = SyntaxFactory.ParseSyntaxTree(source).GetRoot() as CompilationUnitSyntax;
+            var variableDeclaration = compilationUnit.DescendantNodes()
+                                        .OfType<MethodDeclarationSyntax>()
+                                        .Single(m => m.Identifier.ValueText == "foo")
+                                        .DescendantNodes()
+                                        .OfType<VariableDeclarationSyntax>()
+                                        .Single();
+
+            var underTest = new CreateCommentRunnable();
+
+            // When
+            var result = underTest.Run(variableDeclaration).ToFullString();
+
+            // Then
+            Assert.That(result, Is.Not.Null);
+        }
+    }
+}
