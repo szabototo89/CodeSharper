b1df881ec8aec301e786520879afc6de4944d58d
szabototo89 <szabototo89@gmail.com>
2015-08-16 18:37:38 +0200

    Add CombinatorBuilder and start to test selection in complex cases.

---
+++ tests/CodeSharper.Tests.Languages.CSharp/Runnables/CombinatorBuilder.cs
@@ -0,0 +1,28 @@
+﻿using CodeSharper.Core.Nodes.Combinators;
+using CodeSharper.Core.Nodes.Selectors;
+using CodeSharper.Languages.CSharp.Combinators;
+
+namespace CodeSharper.Tests.Languages.CSharp.Runnables
+{
+    public class CombinatorBuilder
+    {
+        private CombinatorBase combinator;
+
+        public CombinatorBuilder Select(SelectorBase selector)
+        {
+            combinator = new SelectionCombinator(selector);
+            return this;
+        }
+
+        public CombinatorBuilder And(SelectorBase selector)
+        {
+            combinator = new RelativeSyntaxNodeCombinator(combinator, new SelectionCombinator(selector));
+            return this;
+        }
+
+        public CombinatorBase Build()
+        {
+            return combinator;
+        }
+    }
+}
---
+++ tests/CodeSharper.Tests.Languages.CSharp/Runnables/ComplexSelectorTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using CodeSharper.Core.Nodes.Combinators;
+using CodeSharper.Languages.CSharp.Combinators;
+using CodeSharper.Languages.CSharp.Selectors.DeclarationSelectors;
+using CodeSharper.Languages.CSharp.Selectors.ExpressionSelectors;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using NUnit.Framework;
+using static CodeSharper.Core.Utilities.ConstructsHelper;
+
+namespace CodeSharper.Tests.Languages.CSharp.Runnables
+{
+    [TestFixture]
+    public class ComplexSelectorTests : TestFixtureBase
+    {
+        public static SyntaxNode ParseDeclaration(String sourceText)
+        {
+            return SyntaxFactory.ParseSyntaxTree(sourceText).GetRoot().ChildNodes().FirstOrDefault();
+        }
+
+        public class RelativeSyntaxNodeCombinatorType : TestFixtureBase
+        {
+            [Test(Description = "should select every classes and every expressions inside the classes")]
+            public void ShouldSelectEveryClassesAndEveryExpressionInsideClasses()
+            {
+                // Arrange
+                var sourceText = @"
+                    public class Foo {
+                        public void FooMethod() {
+                            var i = 10 + 5;
+                        }
+                    }
+                ";
+                var tree = ParseDeclaration(sourceText);
+                var combinator = new CombinatorBuilder()
+                                    .Select(new ClassDeclarationSelector())
+                                    .And(new ExpressionSelector())
+                                    .Build();
+
+                // Act
+                var elements = combinator.Calculate(Array(tree));
+
+                // Assert
+                var result = elements.Select(element => element.ToString());
+                Assert.That(result, Contains.Item("10 + 5"));
+            }
+        }
+    }
+}
